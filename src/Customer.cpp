ecf00882582988db320865a8eb6f506e771069aa
reddydeep868 <ag116@zips.uakron.edu>
2016-05-06 16:27:42 -0400

    copy old code to new method

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -31,7 +31,26 @@ void Customer::addRental(const Rental& rental) {
 
 //Calculates amount for current statement
 	double amountFor(const Rental& r) const{
-		return 0;
+		double thisAmount = 0;
+        switch(it->getVideo().getCode()) {
+
+            case Video::REGULAR:
+            thisAmount += 2;
+            if (it->getDaysRented() > 2)
+                thisAmount += (it->getDaysRented() - 2) * 1.5;
+            break;
+
+            case Video::NEW_RELEASE:
+            thisAmount += it->getDaysRented() * 3;
+            break;
+
+            case Video::CHILDRENS:
+            thisAmount += 1.5;
+            if (it->getDaysRented() > 3)
+                thisAmount += (it->getDaysRented() - 3) * 1.5;
+            break;
+        }
+		return thisAmount;
 	}
 
 // customer rental statement
